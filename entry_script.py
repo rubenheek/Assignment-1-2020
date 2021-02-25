587f7573a61fa3947604349827bc0c8d077b333e
Ruben Heek <rheek@zig.nl>
2021-02-25 17:47:34 +0100

    add comments

--- entry_script.py
+++ entry_script.py
@@ -6,10 +6,12 @@ if __name__ == "__main__":
     '''
     Entry point for the script
     '''
+    # check if the match type is passed
     if len(sys.argv) < 2:
         print("Please provide an argument to indicate which matcher should be used")
         exit(1)
 
+    # check if the match type is a number
     match_type = 0
     try:
         match_type = int(sys.argv[1])
@@ -17,6 +19,7 @@ if __name__ == "__main__":
         print("Match type provided is not a valid number")
         exit(1)
     
+    # check if the match type is a valid option, if so, run the main process method
     if match_type in range(4):
         process.process(match_type)
     else:
